f86fb5e87d8d8c89c1e30ceca510fd4e570a61b3
A00838730 <A00838730@tec.mx>
2024-10-28 23:51:33 -0600

    First Version Portfolio
    This is my first version of my personal portfolio.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      'Prompt': ['Prompt', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
